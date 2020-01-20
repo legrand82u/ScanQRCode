f863e10f9c240bef29e77e3bcfbc109c59089a77
legrand82u <theo.legrand7@etu.univ-lorraine.fr>
2020-01-20 10:02:45 +0100

    Premier commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import QrScanner from 'qr-scanner';
+
+// QrScanner.WORKER_PATH = './qr-scanner-worker.min.js'
+// Set constraints for the video stream
+var constraints = {video: {facingMode: "environment"}, audio: false};// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            const track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    QrScanner.scanImage(document.getElementById("camera--output"))
+        .then(result => alert("QR Code : " + result))
+        .catch(err => alert(err))
+};// Start the video stream when the window loads
+window.addEventListener("load", () => {
+    cameraStart();
+
+    const qrScanner = new QrScanner(document.getElementById("camera--view"), result => alert("QR Code = " + result));
+    qrScanner.start();
+}, false);
